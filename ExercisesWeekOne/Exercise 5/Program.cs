aca13d5cfc0beec17f14a245386253246b7381e4
Roppin.L <robin.ljungqvist@gmail.com>
2016-09-04 20:15:10 +0200

    Exercise 5

---
+++ ExercisesWeekOne/Exercise 5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+
+            var diceThrows = new Dictionary<int, int>();
+
+            for (int i = 1; i < 7 ; i++)
+            {
+                diceThrows.Add(i, 0);
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var diceThrow = rnd.Next(1, 7);
+                diceThrows[diceThrow]++;
+
+            }
+
+            foreach (var pair in diceThrows)
+            {
+                Console.WriteLine($"Value: {pair.Key} Amount: {pair.Value}");
+            }
+
+            var numberOfTries = 0;
+            foreach (var pair in diceThrows)
+            {
+                numberOfTries += pair.Value;
+            }
+            Console.WriteLine($"Number of tries: {numberOfTries}");
+            Console.ReadKey();
+
+        }
+    }
+}
