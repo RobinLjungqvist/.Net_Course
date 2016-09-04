9ef1c68b59bd0da679ad908d336edcb5442adf9d
Roppin.L <robin.ljungqvist@gmail.com>
2016-09-04 23:29:32 +0200

    Exercise 8 and 9

---
+++ ExercisesWeekOne/Exercise 8 and 9/BruteForceSearch.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8_and_9
+{
+    class BruteForceSearch
+    {
+        public bool GetNumber(List<int> numberList, int nrToSearch)
+        {
+            var counter = 0;
+            var sw = new Stopwatch();
+            sw.Start();
+            foreach (var number in numberList)
+            {
+                if(number == nrToSearch)
+                {
+                    sw.Stop();
+                    Console.WriteLine($"You searched for {nrToSearch} it was found and has the index {counter}. It took {sw.Elapsed.TotalMilliseconds}");
+                    return true;
+                }
+                counter++;
+            }
+            Console.WriteLine("Number was not found.");
+            return false;
+
+        }
+
+        public int GetNumberByBinarySearch(List<int> numberList, int nrToSearch)
+        {
+            numberList.Sort();
+
+            var first = numberList[0];
+            var last = numberList[numberList.Count() - 1];
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            while (first <= last)
+            {
+                var mid = (first + last) / 2;
+
+                if(nrToSearch > mid)
+                {
+                    first = mid + 1;
+                }
+                else if(nrToSearch < mid)
+                {
+                    last = mid - 1;
+                }
+                else
+                {
+                    sw.Stop();
+                    if (numberList.IndexOf(mid) >= 1)
+                    {
+
+                        Console.WriteLine("Number was found at index {0} with binary search. It took {1}",
+                                                            numberList.IndexOf(mid), sw.Elapsed.TotalMilliseconds);
+                        
+                    }
+                    
+                    return mid;
+                }
+            }
+            return -1;
+
+
+        }
+
+    }
+}
---
+++ ExercisesWeekOne/Exercise 8 and 9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8_and_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var myIntList = new List<int>();
+                FillCollection(myIntList);
+                var Search = new BruteForceSearch();
+                Search.GetNumber(myIntList, 256);
+                var result = Search.GetNumberByBinarySearch(myIntList, 256);
+                Console.ReadKey(); 
+            }
+        }
+
+        public static void FillCollection(List<int> collection)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < 200; i++)
+            {
+                int tal = rnd.Next(1, 1000);
+                while (collection.Contains(tal))
+                {
+                    tal = rnd.Next(1, 1000);
+                }
+                collection.Add(tal);
+            }
+        }
+    }
+}
