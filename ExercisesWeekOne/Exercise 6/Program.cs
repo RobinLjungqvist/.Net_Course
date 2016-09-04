03628785d33d067ba8df6f97b13afcf22711ab09
Roppin.L <robin.ljungqvist@gmail.com>
2016-09-04 20:55:47 +0200

    Exercise 7

---
+++ ExercisesWeekOne/Exercise 6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Student> studentQueue = new Queue<Student>();
+
+            studentQueue.Enqueue(new Student("Pelle", 25));
+            studentQueue.Enqueue(new Student("Olle", 34));
+            studentQueue.Enqueue(new Student("Danne", 45));
+            studentQueue.Enqueue(new Student("Oskar", 23));
+            studentQueue.Enqueue(new Student("Fredrik", 55));
+
+            foreach (var student in studentQueue)
+            {
+                student.PrintStudent();
+            }
+            do
+            {
+                Console.WriteLine($"{studentQueue.First().FullName} has Graduated!");
+                studentQueue.Dequeue();
+            } while (studentQueue.Count > 0);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ExercisesWeekOne/Exercise 6/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Student
+    {
+        public int Age { get; set; }
+        public string FullName { get; set; }
+
+        public Student(string name, int age)
+        {
+            this.FullName = name;
+            this.Age = age;
+        }
+
+        public void PrintStudent()
+        {
+            Console.WriteLine($"Name: {FullName} Age: {Age}");
+        }
+    }
+}
